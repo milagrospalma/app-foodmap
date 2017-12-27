468df4a7e059f043811ad8d3bd5ca863bcc81427
milagrospalma <mnpalma.2@gmail.com>
2017-12-27 16:59:18 -0500

    Añade data y maquetación de la vista principal

--- js/data.js
+++ js/data.js
@@ -1,6 +1,79 @@
 var data = {
-  'Los Olivos': {
-
+  'Criolla': {
+    'Restaurante1': {
+      'distrito': 'Los Olivos',
+      'mapa': 'url-mapa',
+      'platos': [
+        {
+          'nombre': 'Arroz con pato',
+          'foto': 'url'
+        },
+        {
+          'nombre': 'Papa rellena',
+          'foto': 'url-comida'
+        },
+        {
+          'nombre': 'Ají de gallina',
+          'foto': 'url'
+        }
+      ]
+    },
+    'Restaurante2': {
+      'distrito': 'Independencia',
+      'mapa': 'url-mapa',
+      'platos': [
+        {
+          'nombre': 'Causa rellena',
+          'foto': 'url'
+        },
+        {
+          'nombre': 'Seco de pollo',
+          'foto': 'url-comida'
+        },
+        {
+          'nombre': 'Tacu tacu',
+          'foto': 'url'
+        }
+      ]
+    }
+  },
+  'Selvática': {
+    'El Sheretero': {
+      'distrito': 'Los Olivos',
+      'mapa': 'url-mapa',
+      'platos': [
+        {
+          'nombre': 'Tacacho con cecina',
+          'foto': 'url'
+        },
+        {
+          'nombre': 'Juane',
+          'foto': 'url-comida'
+        },
+        {
+          'nombre': 'Trucha',
+          'foto': 'url'
+        }
+      ]
+    },
+    'El Aguajal': {
+      'distrito': 'Los Olivos',
+      'mapa': 'url-mapa',
+      'platos': [
+        {
+          'nombre': 'Chipado de pescado',
+          'foto': 'url'
+        },
+        {
+          'nombre': 'Juane de gallina',
+          'foto': 'url-comida'
+        },
+        {
+          'nombre': 'Trucha frita',
+          'foto': 'url'
+        }
+      ]
+    }
   }
 
 };
